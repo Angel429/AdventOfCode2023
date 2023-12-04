5a19dcb36987d9e2bd4405b667552478b21c2dc0
Angel429 <angel429a@gmail.com>
2023-12-04 18:02:58 +0100

    Completed Day 4 Part 1

---
+++ Day4/Part2.cs
@@ -0,0 +1,10 @@
+﻿namespace Day4
+{
+    static internal class Part2
+    {
+        public static void Execute()
+        {
+
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,4 @@
+﻿using Day4;
+
+Part1.Execute();
+Part2.Execute();
