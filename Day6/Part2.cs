86cee6f6d078227e93fde3932c7c54e49310478f
Angel429 <angel429a@gmail.com>
2023-12-06 17:07:39 +0100

    Completed Day 6 Part 2

--- Day6/Part2.cs
+++ Day6/Part2.cs
@@ -4,7 +4,15 @@
     {
         public static void Execute()
         {
-            
+            var lines = File.ReadLines("input.txt");
+            var time = long.Parse(lines.First().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Aggregate((prev, next) => prev + next));
+            var distance = long.Parse(lines.Skip(1).First().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Skip(1).Aggregate((prev, next) => prev + next));
+
+            var squareRoot = Math.Sqrt(time * time - 4 * distance);
+            var minimumPressedButtonTime = (int)Math.Ceiling((time - squareRoot) / 2d);
+            var maximumPressedButtonTime = (int)Math.Floor((time + squareRoot) / 2d);
+
+            Console.WriteLine(maximumPressedButtonTime - minimumPressedButtonTime + 1);
         }
     }
 }
