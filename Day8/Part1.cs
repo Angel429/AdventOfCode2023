711992d6a963dcfcaf4a542f2ac9d7fa5e89e58e
Angel429 <angel429a@gmail.com>
2023-12-09 01:24:08 +0100

    Completed Day 8 Part 1

---
+++ Day8/Part1.cs
@@ -0,0 +1,24 @@
+﻿namespace Day8
+{
+    static internal class Part1
+    {
+        public static void Execute()
+        {
+            var linesEnum = File.ReadLines("input.txt");
+
+            var directions = linesEnum.First();
+            var graph = linesEnum.Skip(2).ToDictionary(x => x[0..3], x => (x[7..10], x[12..15]));
+
+            var currentNode = "AAA";
+            var currentDirectionIndex = 0;
+
+            while (currentNode != "ZZZ")
+            {
+                currentNode = directions[currentDirectionIndex % directions.Length] == 'L' ? graph[currentNode].Item1 : graph[currentNode].Item2;
+                currentDirectionIndex++;
+            }
+
+            Console.WriteLine(currentDirectionIndex);
+        }
+    }
+}
---
+++ Day8/Part2.cs
@@ -0,0 +1,10 @@
+﻿namespace Day8
+{
+    static internal class Part2
+    {
+        public static void Execute()
+        {
+
+        }
+    }
+}
