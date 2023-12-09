e434f97e26ce3a27d0a56e45e228b41afdc4e9c6
Angel429 <angel429a@gmail.com>
2023-12-09 18:04:28 +0100

    Completed Day 9 Part 1

---
+++ Day9/Part1.cs
@@ -0,0 +1,41 @@
+﻿namespace Day9
+{
+    static internal class Part1
+    {
+        public static void Execute()
+        {
+            var sum = 0;
+            foreach (var history in File.ReadLines("input.txt"))
+            {
+                var currentSequence = history.Split().Select(int.Parse).ToArray();
+                var allSequences = new List<int[]>
+                {
+                    currentSequence
+                };
+
+                while (currentSequence.Any(x => x != 0))
+                {
+                    var nextSequence = new int[currentSequence.Length - 1];
+
+                    for (int i = 0; i < nextSequence.Length; i++)
+                    {
+                        nextSequence[i] = currentSequence[i + 1] - currentSequence[i];
+                    }
+
+                    allSequences.Add(nextSequence);
+                    currentSequence = nextSequence;
+                }
+
+                var currentNewValue = 0;
+                for (int i = allSequences.Count - 2; i >= 0; i--)
+                {
+                    currentNewValue += allSequences[i][^1];
+                }
+
+                sum += currentNewValue;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Day9/Part2.cs
@@ -0,0 +1,10 @@
+﻿namespace Day9
+{
+    static internal class Part2
+    {
+        public static void Execute()
+        {
+
+        }
+    }
+}
