ac1c7da46aebe95285e8464996ab769751cfde1a
Angel429 <angel429a@gmail.com>
2023-12-11 22:20:21 +0100

    Completed Day 11 Part 2

--- Day11/Part2.cs
+++ Day11/Part2.cs
@@ -4,9 +4,40 @@
     {
         public static void Execute()
         {
-            var map = File.ReadAllLines("input.txt");
+            var image = File.ReadLines("input.txt").Select(x => x.ToCharArray().ToList()).ToList();
 
-            
+            var emptyRows = Enumerable.Repeat(true, image.Count).ToArray();
+            var emptyColumns = Enumerable.Repeat(true, image[0].Count).ToArray();
+            var galaxiesCoords = new List<(int Row, int Column)>();
+
+            for (int rowIndex = 0; rowIndex < emptyRows.Length; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < emptyColumns.Length; columnIndex++)
+                {
+                    if (image[rowIndex][columnIndex] == '#')
+                    {
+                        emptyRows[rowIndex] = false;
+                        emptyColumns[columnIndex] = false;
+                        galaxiesCoords.Add((rowIndex, columnIndex));
+                    }
+                }
+            }
+
+            var sum = 0L;
+
+            for (int galaxy1Index = 0; galaxy1Index < galaxiesCoords.Count; galaxy1Index++)
+            {
+                for (int galaxy2Index = galaxy1Index + 1; galaxy2Index < galaxiesCoords.Count; galaxy2Index++)
+                {
+                    var galaxy1Row = galaxiesCoords[galaxy1Index].Row + 999_999L * emptyRows.Where((x, i) => i < galaxiesCoords[galaxy1Index].Row && x).Count();
+                    var galaxy1Column = galaxiesCoords[galaxy1Index].Column + 999_999L * emptyColumns.Where((x, i) => i < galaxiesCoords[galaxy1Index].Column && x).Count();
+                    var galaxy2Row = galaxiesCoords[galaxy2Index].Row + 999_999L * emptyRows.Where((x, i) => i < galaxiesCoords[galaxy2Index].Row && x).Count();
+                    var galaxy2Column = galaxiesCoords[galaxy2Index].Column + 999_999L * emptyColumns.Where((x, i) => i < galaxiesCoords[galaxy2Index].Column && x).Count();
+                    sum += Math.Abs(galaxy1Row - galaxy2Row) + Math.Abs(galaxy1Column - galaxy2Column);
+                }
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
