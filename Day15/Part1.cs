69eb12c858a27c054aba35d3230ba5f0cb1e1de9
Angel429 <angel429a@gmail.com>
2023-12-17 23:31:08 +0100

    Completed Day 15 Part 1

---
+++ Day15/Part1.cs
@@ -0,0 +1,23 @@
+﻿namespace Day15
+{
+    static internal class Part1
+    {
+        public static void Execute()
+        {
+            var sequence = File.ReadAllLines("input.txt").First();
+
+            var sum = 0;
+            foreach (var step in sequence.Split(','))
+            {
+                var hash = 0;
+                foreach (var character in step)
+                {
+                    hash = ((hash + character) * 17) % 256;
+                }
+                sum += hash;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
